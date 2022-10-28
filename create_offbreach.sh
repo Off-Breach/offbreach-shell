c0f55a3c0e1933dfb873151307b4f67c08e52454
MatheusSs17 <matheus.ssantos@bandtec.com.br>
2022-10-28 15:58:22 -0300

    Fix:(atualização de script)

--- create_offbreach.sh
+++ create_offbreach.sh
@@ -12,49 +12,9 @@ then echo \“Este usuário já está cadastrado\”
 else echo \"O usuário nao existe, criando usuario\"
 sudo adduser offbreach
 sudo usermod -aG sudo offbreach
-sudo apt-get update
+touch ~/.sudo_as_admin_successful 
 su offbreach
 cd
-
-#Atualizando repositórios e pacotes
-echo "Atualizando repositórios e pacotes do sistema"
-sudo apt-get update && sudo apt-get upgrade -y
-
-echo "Deseja instalar a interface grafica? S\n"
-read inst
-if [ \"$inst\"  == \"s\" ]
-then echo \“Instalando a imagem\”
-sudo apt-get install lxde lxde-core xrdp tigervnc-standalone-server lightdm -y  
-fi
-
-#Verificando e instalando o Java
-echo "Verificando versão do Java"
-java --version
-if [ $? -eq 0 ]
-then echo \"O Java já está instalado\"
-else echo \"O Java está não instalado\"
-sudo apt install default-jdk -y
-clear
-echo \"JAVA instalado na versao 11\"
-java --version
-sleep 5
-fi
-
-echo \"Sera realizado agora, a instalacao offbreach, nosso sistema de monitoramento\"
-sleep 4
-clear
-
-sudo apt update && sudo apt upgrade -y
-clear
-wget github.com/Off-Breach/offbreach-install/raw/main/offbreach-1.0-SNAPSHOT-jar-with-dependencies.jar
-sudo apt-get install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo docker pull mysql:5.7
-sudo docker run -d -p 3306:3306 --name containerDB -e "MYSQL_DATABESE=offbreach" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
-sudo docker build -t offbreach-1.0-SNAPSHOT-jar-with-dependencies.jar .
-sudo docker run -d -p 3306:3306 --name offbreach offbreach-1.0-SNAPSHOT-jar-with-dependencies.jar
-clear
 fi
 
 #Atualizando repositórios e pacotes
